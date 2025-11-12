0df1cb8c9cbcb6d27f4602cb41f37cb0e0aa2114
iamdarkle <tomasromero@outlook.com>
2025-11-12 21:13:46 +0100

    chore(2.0): JSON:API changes
    Flarum 2.0 completely changes the JSON:API implementation

--- extend.php
+++ extend.php
@@ -15,6 +15,10 @@ use Flarum\Extend;
 use Flectar\Fancybox\WrapImagesInGallery;
 use Flectar\Fancybox\DefineGalleryTemplate;
 use Flectar\Fancybox\AddExcerptToDiscussion;
+use Flarum\Api\Context;
+use Flarum\Api\Endpoint;
+use Flarum\Api\Resource;
+use Flarum\Api\Schema;
 
 return [
     (new Extend\Frontend('forum'))
@@ -26,6 +30,7 @@ return [
         ->configure(AddExcerptToDiscussion::class)
         ->render(WrapImagesInGallery::class),
  
+    // @TODO: Replace with the new implementation https://docs.flarum.org/2.x/extend/api#extending-api-resources
     (new Extend\ApiSerializer(\Flarum\Api\Serializer\DiscussionSerializer::class))
         ->attribute('excerpt', function ($serializer, $discussion) {
             return $discussion->firstPost ? $discussion->firstPost->formatContent() : null;
